bbd2d11e6b7917b0cb7e0fe87c9e2e7265d65dfe
Epig-is-a-llama <60323417+Epig-is-a-llama@users.noreply.github.com>
2020-01-26 21:48:16 +0000

    removes alot of seperation and depredated stuff
    removes the requirement for the license to be accepted before reading code for reset_stats.py .

--- reset_stats.py
+++ reset_stats.py
@@ -1,65 +1,8 @@
-#In-order to read this code you must have fully agreed with the lisense if you continute to do without agreeing to it, it will be considered an
-#acceptace as you will have seen this message agree to the licesnse!
 
 #Version 0.1.0 Alpha
 
 #By editing this code you accept any dammages to your installation of this progarm or your computer or data held within it. If you are just inrested in reading this code try the notepad sample code as the progarm
 #does nothing with it as it is just there to allow for risk free reading of the sorce code
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 import os
 valid_input = 'false'
