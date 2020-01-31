9e1f96ada53e5c86ea8c67b3aa507ffb860b6093
Epig-is-a-llama <60323417+Epig-is-a-llama@users.noreply.github.com>
2020-01-31 21:06:29 +0000

    Adds the lisense to all major code files
    This change applys to:
    file_create.py
    maths_testing_progarm.py
    reset_stats.py
    logon_system_code.py

--- logon_system_code.py
+++ logon_system_code.py
@@ -1,3 +1,31 @@
+#
+#Version 0.2.0 Alpha
+#
+#By editing this code you accept any dammages to your installation of this progarm or your computer or data held within it. If you are just inrested in reading this code try the notepad sample code as the progarm
+#does nothing with it as it is just there to allow for risk free reading of the sorce code
+#
+#MIT License
+#
+#Copyright (c) 2020 Epig-is-a-llama
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+#
 import os #imports os for the 'exit()' command
 valid_answer = 'No' #prepares the variable used to enshure the user's answer is of use to the prograrm
 print(' ') #to create a space the the input command doesn't seem to like having '\n' in the string it displays
