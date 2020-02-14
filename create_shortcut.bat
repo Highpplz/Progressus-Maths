73bdf363e8e0f36ebd1412bb398662aa12261e96
Epig-is-a-llama <60323417+Epig-is-a-llama@users.noreply.github.com>
2020-02-14 23:46:20 +0000

    Removal of superseeded file

--- create_shortcut.bat
+++
@@ -1,6 +0,0 @@
-echo off
-color e
-title Creating Desktop Shortcut
-echo Once you have opened the folder where the installer is located please right click on the batch file titled 'startgame.bat' and select the 'send to' option and then click 'send to desktop (create shortcut)' once you have done that you can launch the game from there.
-timeout 10
-@shortcut_demo.png
